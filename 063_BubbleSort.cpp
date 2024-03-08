47ad376dbfdb6219a0565dad0735815049275ccc
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-03-08 02:29:36 -0800

    tampilkan data pada number of pass

--- 063_BubbleSort.cpp
+++ 063_BubbleSort.cpp
@@ -31,6 +31,24 @@ void input() {
 	}
 }
 void bubblesortArray() { //procedur untuk mengurutkan array dengan metode bubble sort
-	int
+	int pass = 1; // step 1
+
+	do {
+		for (int j = 0; j <= n - 1 pass; j++) { // step 2
+			if (arr[j] > arr[j + 1]) { // step 3
+				int temp;
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+		pass = pass + 1; // step 4
+
+		cout << "\npass" << pass - 1 << "; "; // number of pass 
+		for (int k = 0; k < n; k++) {
+			cout 
+		}
+	}
+	
 
 }
