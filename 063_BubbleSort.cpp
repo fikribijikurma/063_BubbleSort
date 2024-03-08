3cebcb22887ecab0228b4a6b074f327949f39549
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-03-08 02:05:14 -0800

    memasukan input dari pengguna

---
+++ 063_BubbleSort.cpp
@@ -0,0 +1,18 @@
+// 063_BubbleSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>#
+#include <strip>
+using namespace std;
+
+int arr(20); // Deklarasi variabel global array a dengan ukuran 20
+int n; // deklarasi variabel global n untuk menyimpan banyaknya elemen array
+
+void input() {
+	while (true) {
+		cout << "Masukan banyaknya elemen pada array :  "; // output ke layar
+		cin >> n; // input dari pengguna 
+		if (n <= 20) // jika n kurang dari atau sama dengan 20
+			break; // keluar dari loop
+	}
+}
