a3e858c8fb2a2e434ce2f65a9f2c64e416d779cb
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-03-08 02:13:03 -0800

    masukan output dari pengguna

--- 063_BubbleSort.cpp
+++ 063_BubbleSort.cpp
@@ -14,5 +14,18 @@ void input() {
 		cin >> n; // input dari pengguna 
 		if (n <= 20) // jika n kurang dari atau sama dengan 20
 			break; // keluar dari loop
+		else { // jika n lebih dari 20
+			cout << "\nArray dapat mempunyai maksimal 20 elemen \n"; // output ke layar
+
+		}
+	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukan elemen array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++) { // Looping dengan i dimulai dari 0 hingga n-1
+		cout 
+
 	}
 }
